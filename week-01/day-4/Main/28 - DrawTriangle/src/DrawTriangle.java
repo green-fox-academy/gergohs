28e2125f164bc5a2876701442bec30ffa1f688e1
Horvath Gergo <gergohs@gmail.com>
2018-08-03 17:08:57 +0200

    add excersie 28

---
+++ week-01/day-4/Main/28 - DrawTriangle/src/DrawTriangle.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class DrawTriangle {
+    public static void main(String[] args) {
+
+        Scanner number = new Scanner(System.in);
+
+        int num = number.nextInt();
+        int ln = num;
+        int r;
+
+        for (ln = 0; ln < num; ln++) {
+            System.out.println();
+            for (r = 0; r <= ln; r++) {
+                System.out.print("*");
+            }
+        }
+
+    }
+}
