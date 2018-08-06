504a7a8cae6280cf702b0c5a6bb47866cced3f68
Horvath Gergo <gergohs@gmail.com>
2018-08-06 16:47:14 +0200

    finish task Compare length

--- week-02/day01/Arrays tasks/src/Arrays.java
+++ week-02/day01/Arrays tasks/src/Arrays.java
@@ -1,10 +1,21 @@
 public class Arrays {
     public static void main(String[] args) {
-            System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>EXCERSIES: ARRAYS <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<");
-            System.out.println("***********************************excersie:Third **********************************");
+        System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>EXCERSIES: ARRAYS <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<");
+        System.out.println("***********************************excersie:Third **********************************");
 
-            int[]q = {4, 5, 6, 7};
-            System.out.println(q[3]);
+        int[] q = {4, 5, 6, 7};
+        System.out.println(q[3]);
+
+        System.out.println("***********************************excersie:Compare length **********************************");
+
+        int[] p1 = {1, 2, 3};
+        int[] p2 = {4, 5};
+
+        if (p2.length > p1.length) {
+            System.out.println(true);
+        } else {
+            System.out.println(false);
+        }
 
     }
 }
