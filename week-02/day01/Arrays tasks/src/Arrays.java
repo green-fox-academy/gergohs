72a991dceae8c468a0208acda05a119d07547d3c
Horvath Gergo <gergohs@gmail.com>
2018-08-06 16:37:05 +0200

    finish Array task: Third

---
+++ week-02/day01/Arrays tasks/src/Arrays.java
@@ -0,0 +1,10 @@
+public class Arrays {
+    public static void main(String[] args) {
+            System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>EXCERSIES: ARRAYS <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<");
+            System.out.println("***********************************excersie:Third **********************************");
+
+            int[]q = {4, 5, 6, 7};
+            System.out.println(q[2]);
+
+    }
+}
--- week-02/day01/src/week2day1.java
+++ week-02/day01/src/week2day1.java
@@ -1,23 +1,56 @@
 public class week2day1 {
 
     public static void main(String[] args) {
-    // innen kell hívni a függvényeket
-        System.out.println("**********************excersie 1: doubling********************");
+        // innen kell hívni a függvényeket
+        System.out.println("*********************************** excersie 1: doubling  **********************************");
 
         int baseNum = 123;
 
         int returnNum = doubling(baseNum);
         System.out.println(returnNum);
 
+        System.out.println("*********************************** excersie 2: greetings **********************************");
+
+        String a1 = "Greenfox";
+
+        String a2 = greet(a1);
+
+        System.out.println(a2);
+
+        System.out.println("*********************************** excersie 3: Append A **********************************");
+
+        String typo = "Chinchill";
+
+        String typo2 = appendAFunc(typo);
+        System.out.println(typo2);
+
     }
     // ide kellenek a függvények (MAIN függvény után)
-System.out.println("**********************excersie 1: doubling********************");
+
+    //***********************************excersie 1: doubling ********************************************************
+
     public static int doubling(int input) {
 
-        int doublebaseNum = input*2;
+        int doublebaseNum = input * 2;
         return doublebaseNum;
     }
-System.out.println("**********************excersie 2: ********************");
+    //***********************************excersie 2: greeting ********************************************************
+
+    public static String greet(String greeting) {
+
+        String attach = "Greetings dear, " + greeting;
+
+        return attach;
+    }
+    //***********************************excersie 3: Append a ********************************************************
+
+    public static String appendAFunc(String typoFromMain) {
+
+        String add = "a";
+        String add2 = typoFromMain + add;
+
+        return add2;
+    }
 
 
 }
