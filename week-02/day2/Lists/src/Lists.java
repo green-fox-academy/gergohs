7a0f9fee36b7995b2b86fb2aa6d49dd1ed027483
Horvath Gergo <gergohs@gmail.com>
2018-08-07 16:45:07 +0200

    finish task solarSystem

---
+++ week-02/day2/Lists/src/Lists.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Lists {
+    public static void main(String[] args) {
+
+        ArrayList<String> planetList = new ArrayList<String>(Arrays.asList
+                ("Mercury", "Venus", "Earth", "Mars", "Jupiter", "Uranus", "Neptune"));
+
+        System.out.println(putSaturn(planetList));
+    }
+
+    private static ArrayList putSaturn(ArrayList planetList) {
+
+        planetList.add(5, "Saturn");
+
+        return planetList;
+    }
+}
