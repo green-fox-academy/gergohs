3d5bc40377b5c0653977704ba9f35fc1a464a6bd
Horvath Gergo <gergohs@gmail.com>
2018-08-07 15:44:31 +0200

    Revert "finish task reverse reversed"
    This reverts commit 1f1b0e95fdd9319cbc7fdf28a777ff5e190cd8a5.

--- week-02/day2/src/Strings.java
+++ week-02/day2/src/Strings.java
@@ -1,7 +1,6 @@
 import javax.xml.bind.SchemaOutputResolver;
 
 public class Strings {
-
     public static void main(String[] args) {
 
         System.out.println("***************************************  simple replacement ***************************************");
@@ -24,7 +23,7 @@ public class Strings {
 
         String quote = "Hofstadter's Law: It you expect, even when you take into account Hofstadter's Law.";
 
-        System.out.println(quote.replace("It you ", "It always takes longer than you "));
+       System.out.println(quote.replace("It you ", "It always takes longer than you "));
 
         System.out.println("*************************************** todo print ***************************************");
 
@@ -33,37 +32,11 @@ public class Strings {
         StringBuilder str = new StringBuilder("- Buy milk");
 
         str.insert(0, "My todo: \n");
-
+        
         todoText = (str.insert((str.length()), "\n - Download game \n \t - Diabolo")).toString();
 
         System.out.println(todoText);
 
-        System.out.println("*************************************** reverse ***************************************");
 
-        String reversed = ".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI";
-
-
-        // solution using the charAt() -- method find below
-
-        System.out.println(reverse(reversed));
-
-
-//        this is an anything else solution for the same task:
-       String rev = new StringBuilder(reversed).reverse().toString();
-
-        System.out.println(rev);
     }
-
-    // method for reverse reversed
-
- public static String reverse (String reversed) {
-
-        String reverse = "";
-     for (int i = (reversed.length() - 1); i >= 0; i--) {
-         reverse += reversed.charAt(i);
-     }
-     return reverse;
- }
-
 }
-
