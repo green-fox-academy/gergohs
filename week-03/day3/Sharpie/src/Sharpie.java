2aa0819bc23851a7dba0d03db70b3e0476b941f2
Horvath Gergo <gergohs@gmail.com>
2018-08-16 13:14:35 +0200

    feat(Sharpie): finish task

---
+++ week-03/day3/Sharpie/src/Sharpie.java
@@ -0,0 +1,16 @@
+public class Sharpie {
+
+    String color;
+    float width;
+    int inkAmount = 100;
+
+
+    public  Sharpie(){
+
+    }
+
+    public void use () {
+        inkAmount--;
+    }
+
+}
---
+++ week-03/day3/Sharpie/src/mySharpies.java
@@ -0,0 +1,37 @@
+public class mySharpies {
+    public static void main(String args[]) {
+
+        Sharpie sharpie1 = new Sharpie();
+
+        String sharpie1Color = "green";
+        float sharpie1Width =  2.5f;
+
+        int numOfwrite1 = 26;
+
+        for (int i = 0; i < numOfwrite1; i++) {
+            sharpie1.use();
+        }
+
+        sharpie1.use();
+        if (sharpie1.inkAmount <= 0) {
+            System.out.println("The " + sharpie1Color + " sharpie is out of ink.");
+        } else {
+            System.out.println("Ink amount of the " + sharpie1Color + " sharpie is " + sharpie1.inkAmount);
+        }
+
+//*****************************************************************************************
+
+        Sharpie sharpie2 = new Sharpie();
+
+        String sharpie2Color = "blue";
+        float sharpie2Width = 5;
+
+        int numOfwrite2 = 32;
+
+        for (int i = 0; i < numOfwrite2; i++) {
+            sharpie2.use();
+        }
+        System.out.println("Ink amount of the " + sharpie2Color + " sharpie is " + sharpie2.inkAmount);
+        
+    }
+}
