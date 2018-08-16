96854eaaf7ff57c9f1d36b16a8bd0631d001b2f4
Horvath Gergo <gergohs@gmail.com>
2018-08-16 12:21:37 +0200

    fix(task animal)

--- week-03/day3/animal/src/animal.java
+++ week-03/day3/animal/src/animal.java
@@ -7,15 +7,15 @@ public class animal {
     }
 
     public void eat() {
-        this.hunger = hunger + 1;
+        this.hunger = hunger - 1;
     }
 
     public void drink() {
-        this.thirst = thirst + 1;
+        this.thirst = thirst - 1;
     }
 
     public void play() {
-        hunger--;
-        thirst--;
+        hunger++;
+        thirst++;
     }
 }
