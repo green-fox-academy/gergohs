066004d63b84d6631d94263f523554d6cfdb0553
Horvath Gergo <gergohs@gmail.com>
2018-09-12 17:31:07 +0200

    feat(print even): finish task

---
+++ week-06/day03/print-even/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+// Create a program that prints all the even numbers between 0 and 500
+
+int main()
+{
+
+    for (int i = 0; i<=500; i++)
+    {
+        if (i%2==0)
+        {
+            printf("%d \n", i);
+        }
+    }
+
+}
