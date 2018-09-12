9091332e79fdc2232746faca497b9b4f584240f9
Horvath Gergo <gergohs@gmail.com>
2018-09-12 20:51:30 +0200

    feat(sumElements+miltiplicationTable): finish tasks

---
+++ week-06/day03/sum-elements/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+// - Create an array variable named `r`
+//   with the following content: `[54, 23, 66, 12]`
+// - Print the sum of the second and the third element
+
+//int sum array(int r[], int num_elements)
+
+int main()
+{
+    int r[4] = {54, 23, 66, 12};
+
+    printf("%d\n", r[1]+r[2]);
+
+}
+
+
