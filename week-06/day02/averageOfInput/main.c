61f56ec3b3af58c35679495fcc0d0ef091196bc9
Horvath Gergo <gergohs@gmail.com>
2018-09-12 15:41:53 +0200

    feat(averageOfInput): finish task

---
+++ week-06/day02/averageOfInput/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+// Write a program that asks for 5 integers in a row,
+// then it should print the sum and the average of these numbers like:
+// Sum: 22, Average: 4
+
+int main()
+{
+    int a;
+    int b;
+    int c;
+    int d;
+    int e;
+
+    printf("Please enter 5 numbers: \n");
+    scanf("%d %d %d %d %d", &a, &b, &c, &d, &e);
+    printf("Sum: %d, Average: %d \n", a+b+c+d+e, (a+b+c+d+e)/5);
+    //printf("Average: %d \n", (a+b+c+d+e)/5);
+
+}
