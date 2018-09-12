27a3605f993552e46fec9206e6b469d3f67e4485
Horvath Gergo <gergohs@gmail.com>
2018-09-12 17:25:17 +0200

    feat(i wont cheat): finish task

---
+++ week-06/day03/i-wont-cheat/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+// Create a program that writes this line 100 times:
+// "I won't cheat on the exam!"
+int main()
+{
+    int i = 0;
+    for (i; i <100;i++){
+        printf("I won't cheat on exams \n");
+    }
+
+
+}
