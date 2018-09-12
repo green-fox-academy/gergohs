d164f14f3572666cee1d722d46b46ed3c185f2a5
Horvath Gergo <gergohs@gmail.com>
2018-09-12 13:11:37 +0200

    feat(favorite-number): finish task

---
+++ week-06/day02/farorite-number/main.c
@@ -0,0 +1,14 @@
+    // Store your favorite number in a variable (as a number)
+    // And print it like this: "My favorite number is: 8"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+int number = 17;
+
+printf("My favorite number is %d! \n", number);
+
+    return 0;
+}
