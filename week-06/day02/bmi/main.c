1a0c59e50f136c800e6eb6c61555cde8720f6038
Horvath Gergo <gergohs@gmail.com>
2018-09-12 14:20:25 +0200

    feat(BMI): finish task

---
+++ week-06/day02/bmi/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    double massInKg = 65;
+    double heightInM = 1.82;
+
+    printf("The BMI index is: %.2f \n", massInKg/heightInM);
+
+    return 0;
+}
