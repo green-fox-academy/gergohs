5b2c00fcc3408e91df49a09d5c7544a0cf351357
Horvath Gergo <gergohs@gmail.com>
2018-09-12 13:31:42 +0200

    feat(define-basic-info): finish task

---
+++ week-06/day02/define-basic-info/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+char firstletter = 'G';
+int age = 32;
+double height = 1.82;
+unsigned int favNum= 17;
+
+printf("First letter of my name is %c \n", firstletter);
+printf("My age is %d \n", age);
+printf("My height is %.2f \n", height);
+printf("My favorite number is %u \n", favNum);
+
+    return 0;
+}
