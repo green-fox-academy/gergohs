f36d2e0e1440cffc241e4eeda903f30c62cd1e70
Horvath Gergo <gergohs@gmail.com>
2018-09-12 12:22:50 +0200

    feat(introduce-yourself): finish task

---
+++ week-06/day02/introduce-yourself/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+char name[] = "Gergo";
+int age = 32;
+float height = 1.82;
+
+printf("%s \n", name);
+printf("%d \n", age);
+printf("%.2f \n", height);
+
+    return 0;
+}
