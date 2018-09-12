96006fc5ceeeae3c1711d6d33d0bee0a350ac37e
Horvath Gergo <gergohs@gmail.com>
2018-09-12 14:42:59 +0200

    feat(hello-user): finish task

---
+++ week-06/day02/hello-user/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+char name[10];
+
+printf("Please enter your name: \n");
+scanf("%s", name);
+
+printf("Hello, %s! \n", name);
+
+
+return 0;
+}
