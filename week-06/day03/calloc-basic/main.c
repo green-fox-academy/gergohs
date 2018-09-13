6c6debc9f1a89d396f52154e309eacb4382aa1e7
Horvath Gergo <gergohs@gmail.com>
2018-09-13 15:53:58 +0200

    feat(calloc-basic): finish task

---
+++ week-06/day03/calloc-basic/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// with calloc
+// please allocate a 10 long array and fill it with 5!! numbers from 0 to 4, then print the whole array
+// please delete the array before the program exits
+// what's the difference between this and the previous excersice?
+
+int main()
+{
+    int* pointer = NULL;
+
+    pointer = (int*)calloc(10, sizeof(int));
+    for (int i = 0; i < 5; i++){
+        pointer [i] = i;
+        printf ("%d, ", pointer[i]);
+    }
+
+    free(pointer);
+
+    return 0;
+}
