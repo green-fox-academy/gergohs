8cad9cef33726402729beed635ee4f0ce7471ae8
Horvath Gergo <gergohs@gmail.com>
2018-09-13 23:41:51 +0200

    printf for demo :)

---
+++ week-06/day03/no-school/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("\n\n\n\n\n\n\n\t\t\t*******************************************************************************");
+    printf("\n\n\n\t\t\t\t\t\t C programming language");
+    printf("\n\n\n\t\t\t*******************************************************************************");
+    printf("\n\n\t\t\t\tThere is\n \t\t\t\t\t\tno school\n\t\t\t\t\t\t\t\t like the \n\t\t\t\t\t\t\t\t\t\t old school!");
+    printf("\n\n\t\t\t*******************************************************************************");
+    printf("\n\n\n\t\t\t\t\t Thank you for your attention and have a nice weekend");
+    printf("\n\n\n\t\t\t*******************************************************************************\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+
+    return 0;
+}
