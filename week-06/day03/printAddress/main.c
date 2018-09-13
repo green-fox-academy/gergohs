d15be2d393ded680c476e1010e19f61e12b99c51
Horvath Gergo <gergohs@gmail.com>
2018-09-13 11:50:40 +0200

    feat(printAddress): finish task

---
+++ week-06/day03/printAddress/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+// print the memory address of the variable
+
+int main()
+{
+    int number = 1234;
+    int *pnum = &number;
+    printf("Address: %p\n", number, &number, *pnum, pnum);
+}
