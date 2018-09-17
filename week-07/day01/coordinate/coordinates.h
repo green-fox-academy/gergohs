56d1d01630344796152c5461663996d046c35ce5
Horvath Gergo <gergohs@gmail.com>
2018-09-17 20:56:34 +0200

    feat(coordinate): finish task

---
+++ week-07/day01/coordinate/coordinates.h
@@ -0,0 +1,11 @@
+#ifndef COORDINATES_H_INCLUDED
+#define COORDINATES_H_INCLUDED
+#include <stdio.h>
+
+typedef struct
+{
+    int x;
+    int y;
+} Coordinates;
+
+#endif // COORDINATES_H_INCLUDED
---
+++ week-07/day01/coordinate/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "coordinates.h"
+
+// Create a struct to store coordinates (for example: int x and int y)
+// Create an array which can store coordinates, do not create multidimensonal arrays here
+// Fill up the coordinates in the array with random number.
+
+
+int main()
+{
+    Coordinates coordinateArr[20];
+
+    int max = 101;
+    for (int i=0; i<sizeof(coordinateArr)/sizeof(Coordinates); i++)
+    {
+        coordinateArr[i].x = (rand() % max+1);
+        coordinateArr[i].y = (rand() % max+1);
+        printf("The coordinates of the %d element are: %d, %d \n", i, coordinateArr[i].x, coordinateArr[i].y);
+    }
+}
