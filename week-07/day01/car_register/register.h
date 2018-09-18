f9e61a7023e2753bc7e108db009c779fe6d66118
Horvath Gergo <gergohs@gmail.com>
2018-09-18 16:12:47 +0200

    feat(car register): creat main.c

---
+++ week-07/day01/car_register/register.h
@@ -0,0 +1,16 @@
+#ifndef REGISTER_H_INCLUDED
+#define REGISTER_H_INCLUDEDû
+#include <stdio.h>
+#include <stdlib.h>
+
+enum transmission {manual, automatic, CVT, semi_automatic, dual_clutch} transmission;
+
+typedef struct
+{
+    char manufacturer[256];
+    float price_in_euros;
+    int year_of_manufact;
+    enum transmission;
+}
+
+#endif // REGISTER_H_INCLUDED
