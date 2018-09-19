77ec89aa39db148dbd1fd0ef8052ac13c3ed04e8
Horvath Gergo <gergohs@gmail.com>
2018-09-19 16:58:11 +0200

    feat(pokemon): style main.c

--- week-07/day01/pokemon/main.c
+++ week-07/day01/pokemon/main.c
@@ -9,19 +9,23 @@ int main()
     Pokemons pokemon5;
 
     pokemon1.pokemon_type = water;
-    pokemon1.speed = 6.5;
+    pokemon1.speed = 6.0;
     pokemon1.strength = 3.8;
-    pokemon2.pokemon_type = water;
-    pokemon2.speed = 9.8;
+
+    pokemon2.pokemon_type = flying;
+    pokemon2.speed = 9.0;
     pokemon2.strength = 2.5;
+
     pokemon3.pokemon_type = fire;
     pokemon3.speed = 2.0;
     pokemon3.strength = 8.9;
-    pokemon4.pokemon_type = fire;
-    pokemon4.speed = 4.9;
-    pokemon4.strength = 5.1;
+
+    pokemon4.pokemon_type = dragon;
+    pokemon4.speed = 4.0;
+    pokemon4.strength = 9.8;
+
     pokemon5.pokemon_type = fire;
-    pokemon5.speed = 2.2;
+    pokemon5.speed = 2.0;
     pokemon5.strength = 1.9;
 
     Pokemons pokemon_list[] = {pokemon1, pokemon2, pokemon3, pokemon4, pokemon5};
