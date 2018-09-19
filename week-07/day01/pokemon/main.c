7088c29af4cf622db4b854bf90289ec90f86b89f
Horvath Gergo <gergohs@gmail.com>
2018-09-19 14:57:22 +0200

    feat(pokemon): initialize pokemons, create list, call methods

--- week-07/day01/pokemon/main.c
+++ week-07/day01/pokemon/main.c
@@ -1,8 +1,35 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "pokemon.h"
 
 int main()
 {
-    printf("Hello world!\n");
-    return 0;
+    Pokemons pokemon1;
+    Pokemons pokemon2;
+    Pokemons pokemon3;
+    Pokemons pokemon4;
+    Pokemons pokemon5;
+
+    pokemon1.pokemon_type = water;
+    pokemon1.speed = 6.5;
+    pokemon1.strength = 3.8;
+    pokemon2.pokemon_type = water;
+    pokemon2.speed = 9.8;
+    pokemon2.strength = 2.5;
+    pokemon3.pokemon_type = fire;
+    pokemon3.speed = 2.0;
+    pokemon3.strength = 8.9;
+    pokemon4.pokemon_type = fire;
+    pokemon4.speed = 4.9;
+    pokemon4.strength = 5.1;
+    pokemon5.pokemon_type = fire;
+    pokemon5.speed = 2.2;
+    pokemon5.strength = 1.9;
+
+    Pokemons pokemon_list[] = {pokemon1, pokemon2, pokemon3, pokemon4, pokemon5};
+
+    int array_length = sizeof(pokemon_list)/sizeof(pokemon_list[0]);
+
+    get_faster(pokemon_list, array_length, 3.0);
+    get_type(pokemon_list, array_length, fire);
+    get_strongest(pokemon_list, array_length, 5.0);
+    get_max_strength(pokemon_list, array_length, water);
 }
