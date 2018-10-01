b51d39a69303e2d5ef7c735962c4ed4395699719
Horvath Gergo <gergohs@gmail.com>
2018-10-01 14:52:01 +0200

    feat(fibonacci): call functions in main.c

--- week-08/fibonacci/main.c
+++ week-08/fibonacci/main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include  "fibonacci.h"
+
 
 /* Write a function which is called "get_fibonacci_number". It returns the "N"th
  * Fibonacci number, where "N" is received as a paramter.
@@ -17,29 +19,14 @@
 int main()
 {
     int given_num;
+    int element;
     printf("Please enter a number\n");
     scanf("%d", &given_num);
 
+    printf("Please give the element what you want to find: ");
+    scanf("%d", &element);
+
     generate_fibonacci(given_num);
-}
-
-
-
-int generate_fibonacci(int given_num)
-{
-    printf("The first %d elements of the Fibonacci sequence is: \n", given_num);
-    int a=0;
-    int b=1;
-    int c;
-
-    for (int i = 1; i<=given_num; i++)
-    {
-        printf("%d, ", a);
-        c = a+b;
-        a=b;
-        b=c;
-    }
-    printf("\n");
-    return 0;
+    find_nth_element(element);
 }
 
