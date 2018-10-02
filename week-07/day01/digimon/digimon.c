b9372eb3f89982596451b67f91de709e20267ce1
Horvath Gergo <gergohs@gmail.com>
2018-10-02 12:06:15 +0200

    feat(digimon): create functions: min health index and digivol level counter

--- week-07/day01/digimon/digimon.c
+++ week-07/day01/digimon/digimon.c
@@ -1,18 +1,36 @@
-#include "diginom.h"
+#include "digimon.h"
 
 
-void min_health_index (Digimon digimon_list, array_length)
+int min_health_index (Digimon *digi_list, int size)
 {
-    int min_health = digimon_list[0].healt;
+    int min_health = digi_list[0].healt;
     int min_index = 0;
 
-    for (int i=0; i<array_length; i++)
+    for (int i=0; i<size; i++)
     {
-        if (digimon_list[i].healt < min_health)
+        if (digi_list[i].healt < min_health)
         {
-            min_health = digimon_list[i].healt;
+            min_health = digi_list[i].healt;
             min_index = i;
         }
     }
-    printf("The minimum health level is: %d\n", min_index);
+    printf("The index of the digimon which has the minimum health level: %d\n\n", min_index);
 }
+//*********************************************************************************************************
+//*********************************************************************************************************
+
+int digivol_level_counter (Digimon *digi_list, int size, Digivolution digivol_level)
+{
+
+    int counter = 0;
+    for (int i = 0; i < size; i++){
+        if (digi_list[i].digivol_level == digivol_level){
+            counter++;
+        }
+    }
+        return printf("The number of the digimons int the given level: %d \n", counter);
+}
+//*********************************************************************************************************
+//*********************************************************************************************************
+
+
--- week-07/day01/digimon/main.c
+++ week-07/day01/digimon/main.c
@@ -1,10 +1,41 @@
-#include "diginom.h"
+#include "digimon.h"
 
 int main()
 {
+    Digimon digimon1;
+    Digimon digimon2;
+    Digimon digimon3;
+    Digimon digimon4;
+    Digimon digimon5;
+    Digimon digimon6;
+
+    digimon1.healt = 50;
+    digimon1.digivol_level = baby;
+
+    digimon2.healt = 40;
+    digimon2.digivol_level = baby;
+
+    digimon3.healt = 10;
+    digimon3.digivol_level = baby;
+
+    digimon4.healt = 25;
+    digimon4.digivol_level = champion;
+
+    digimon5.healt = 49;
+    digimon5.digivol_level = ultimate;
+
+    digimon6.healt = 55;
+    digimon6.digivol_level = mega;
+
+    Digimon digi_list[] = {digimon1, digimon2, digimon3, digimon4, digimon5, digimon6};
 
 
 
-    Digimon diginom1;
+    int size = sizeof(digi_list)/sizeof(digi_list[0]);
+    Digivolution digivol_level = training;
+
+
+    min_health_index(digi_list, size);
+    digivol_level_counter(digi_list, size, digivol_level);
 
 }
