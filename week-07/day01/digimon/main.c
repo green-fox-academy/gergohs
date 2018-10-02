f27cacfd4ac4882b53300d566b9bfde79035d94c
Horvath Gergo <gergohs@gmail.com>
2018-10-02 13:14:07 +0200

    feat(digimon): call functions, format code

--- week-07/day01/digimon/main.c
+++ week-07/day01/digimon/main.c
@@ -11,31 +11,36 @@ int main()
 
     digimon1.healt = 50;
     digimon1.digivol_level = baby;
+    strcpy(digimon1.name_of_tamer, "monster");
 
     digimon2.healt = 40;
     digimon2.digivol_level = baby;
+    strcpy(digimon2.name_of_tamer, "fighter");
 
     digimon3.healt = 10;
     digimon3.digivol_level = baby;
+    strcpy(digimon3.name_of_tamer, "wonder");
 
     digimon4.healt = 25;
     digimon4.digivol_level = champion;
+    strcpy(digimon4.name_of_tamer, "monster");
 
     digimon5.healt = 49;
     digimon5.digivol_level = ultimate;
+    strcpy(digimon5.name_of_tamer, "wonder");
 
     digimon6.healt = 55;
     digimon6.digivol_level = mega;
+    strcpy(digimon6.name_of_tamer, "wonder");
 
     Digimon digi_list[] = {digimon1, digimon2, digimon3, digimon4, digimon5, digimon6};
 
-
-
     int size = sizeof(digi_list)/sizeof(digi_list[0]);
-    Digivolution digivol_level = training;
-
+    Digivolution digivol_level = ultimate;
+    char tamer[128] = "monster";
 
     min_health_index(digi_list, size);
     digivol_level_counter(digi_list, size, digivol_level);
-
+    tamer_count(digi_list, size, tamer);
+    same_tamer_avg (digi_list, size, tamer);
 }
