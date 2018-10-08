680c962a1bb85dfdb13d5a3889e88f6f541ade65
Horvath Gergo <gergohs@gmail.com>
2018-10-08 21:52:55 +0200

    feat(persons_solution): .h and .c file

--- week-10/age_groups_practice/persons.c
+++ week-10/age_groups_practice/persons.c
@@ -1,12 +1,1 @@
-#include "persons.h"
 
-persons_t* get_persons(const char* file_name, int* number_of_persons)
-{
-
-}
-
-
-void list_persons_in_age_groups(persons_t* persons, int number_of_persons, age_groups age_group)
-{
-
-}
